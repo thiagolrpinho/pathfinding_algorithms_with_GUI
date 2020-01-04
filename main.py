a9491313c984713e25d5e42ee3910001f91c867b
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-04 16:38:35 -0300

    :tada: Succesfully created empty canvas

---
+++ main.py
@@ -0,0 +1,10 @@
+''' Main module of the pathfinding project. 
+Used to show graphic interface.
+'''
+
+import pygame
+pygame.init()
+
+
+pygame_window = pygame.display.set_mode((400, 400))
+pygame.quit()
